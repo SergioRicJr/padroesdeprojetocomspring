2dea084e427bab339d6fb854c71c8b14d575edd0
Sergio <sergioricjr7@gmail.com>
2023-04-14 14:42:59 -0300

    final

--- src/main/java/one/digitalinnovation/padroesprojetospring/PadroesprojetospringApplication.java
+++ src/main/java/one/digitalinnovation/padroesprojetospring/PadroesprojetospringApplication.java
@@ -2,12 +2,13 @@ package one.digitalinnovation.padroesprojetospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PadroesprojetospringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PadroesprojetospringApplication.class, args);
 	}
-
 }
